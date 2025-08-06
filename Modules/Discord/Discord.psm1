a3c1dd9ed6718f5ecf22e45ec74272275408ac65
Nikolaj Petersen <n@nipetersen.dk>
2025-08-06 04:20:50 +0200

    Ensart Discord log beskeder mellem løsninger

--- Modules/Discord/Discord.psm1
+++ Modules/Discord/Discord.psm1
@@ -1,8 +1,18 @@
 function Send-DiscordMessage {
     param(
         [Parameter(Mandatory=$true)][string]$WebhookUrl,
-        [Parameter(Mandatory=$true)][string]$Text
+        [Parameter(Mandatory=$true)][string]$Text,
+        [Parameter(Mandatory=$false)][bool]$EnableDebug = $false
     )
+
+    $LogTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    if ($EnableDebug -eq $true) {
+        $Text = "$($LogTime): DBG: $Text"
+    }
+    else {
+        $Text = "$($LogTime): $Text"
+    }
     $body = @{ content = "$Text" } | ConvertTo-Json
     Invoke-RestMethod -Uri $WebhookUrl -Method Post -ContentType 'application/json' -Body $body
 }
